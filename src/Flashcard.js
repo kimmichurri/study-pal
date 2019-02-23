ccfd1e537cc6425481d05ef293a68e1922368c5b
“Kimberly <kimberly.achkio@gmail.com>
2019-02-23 16:17:18 -0700

    Insert and style flashcard component

---
+++ src/Flashcard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Flashcard () {
+    return (
+        <article>
+            <p className="flashcard-container">I am a Flashcard! I will hold the questions and answers here.</p>
+        </article>
+    )
+}
