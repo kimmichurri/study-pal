0f0b5f4ae8a5d540fe9da4327847a70ffd371a1b
“Kimberly <kimberly.achkio@gmail.com>
2019-02-24 15:11:47 -0700

    Change answer choices from p tags to button tags

--- src/DisplayQuestionsAndAnswers.js
+++ src/DisplayQuestionsAndAnswers.js
@@ -6,9 +6,11 @@ export default function DisplayQuestionsAndAnswers (props) {
     return (
         <section>
             <p className="question-text">{props.flashcard.question}</p>
-            <p className="answer-text">{props.flashcard.answerChoices[0]}</p>
-            <p className="answer-text">{props.flashcard.answerChoices[1]}</p>
-            <p className="answer-text">{props.flashcard.answerChoices[2]}</p>
+            <section className="answer-choice-buttons-container">
+                <button className="answer-text">{props.flashcard.answerChoices[0]}</button>
+                <button className="answer-text">{props.flashcard.answerChoices[1]}</button>
+                <button className="answer-text">{props.flashcard.answerChoices[2]}</button>
+            </section>
         </section>
     )
 }
