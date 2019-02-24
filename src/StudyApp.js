914ca9a3177565adf955d155fd19468c20170dfe
“Kimberly <kimberly.achkio@gmail.com>
2019-02-24 10:48:36 -0700

    Set state to array questions on click

--- src/StudyApp.js
+++ src/StudyApp.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import brainLogo from './assets/brainimg.png';
 import Header from './Header'
 import Flashcard from './Flashcard'
-import PrototypeMethodsData from './PrototypeMethodsData'
+import { stringAndArrayPrototypeMethods } from './PrototypeMethodsData'
 
 
 export default class StudyApp extends Component {
     constructor() {
         super();
         this.state = {
-            questionCategory: null,
-            flashcards: [],
-            studyBank: [],
-            showButtonsAndLogo: true,
-            showFlashCard: false
+            flashcardData: stringAndArrayPrototypeMethods,
+            flashcards: []
+            // studyBank: []
+            // in component did mount set the state
         }
     }
 
-    // chooseStudyCategory = () => {
-    //      user clicks on either string, array, or archive 
-    //      set state this.showButtonsAndLogo: false;
-    // }
+    chooseArrayCategory = (e) => {
+        if (e.target.innerText === 'Array Prototype Methods') {
+            let arrayQuestions = this.state.flashcardData.filter((question) => {
+                console.log(question.category)
+                return question.category === "Array Methods"
+            })
+            this.setState({
+                flashcards: arrayQuestions
+            })
+        }   
+    }
 
     render() {
         return (
@@ -29,7 +35,7 @@ export default class StudyApp extends Component {
                 <img src={brainLogo} className="study-pal-logo" alt="logo" />
                 <section className= "study-choice-buttons-container">
                     <button className="study-choice-buttons">Study Bank</button>
-                    <button className="study-choice-buttons">Array Prototype Methods</button>
+                    <button onClick={this.chooseArrayCategory} className="study-choice-buttons">Array Prototype Methods</button>
                     <button className="study-choice-buttons">String Prototype Methods</button>
                 </section>
                 <Flashcard />
