eb47bae2eae1a14706b0575b5045e3d9443d1d65
“Kimberly <kimberly.achkio@gmail.com>
2019-02-24 19:26:21 -0700

    Hook up validate answer method to answer buttons

--- src/DisplayQuestionsAndAnswers.js
+++ src/DisplayQuestionsAndAnswers.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export default function DisplayQuestionsAndAnswers (props) {
-
    
     return (
         <section>
             <p className="question-text">{props.flashcard.question}</p>
             <section className="answer-choice-buttons-container">
-                <button className="answer-text">{props.flashcard.answerChoices[0]}</button>
-                <button className="answer-text">{props.flashcard.answerChoices[1]}</button>
-                <button className="answer-text">{props.flashcard.answerChoices[2]}</button>
+                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[0]}</button>
+                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[1]}</button>
+                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[2]}</button>
             </section>
         </section>
     )
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -3,6 +3,13 @@ import DisplayQuestionsAndAnswers from './DisplayQuestionsAndAnswers'
 
 export default class Flashcard extends Component {
 
+    validateAnswer =(e) => {
+        if (e.target.innerText === this.props.flashcards[0].correctAnswer) {
+            console.log("You're Right!")
+        } else {
+            console.log("Sorry, that is incorrect!")
+        }
+    }
     
     // currentQuestion() {
     //     console.log(this.state.currentFlashcards)
@@ -11,7 +18,10 @@ export default class Flashcard extends Component {
     render() {
         return (
             <article className="flashcard-container">
-                <DisplayQuestionsAndAnswers flashcard = {this.props.flashcards[0]} />
+                <DisplayQuestionsAndAnswers 
+                    flashcard={this.props.flashcards[0]} 
+                    validateAnswer={this.validateAnswer}
+                    />
             </article>
         )
     }
