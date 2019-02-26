bdd6fe4fc1ec7484fc67cde21e341f5b8a64f61b
“Kimberly <kimberly.achkio@gmail.com>
2019-02-25 17:02:38 -0700

    Add notice with component after displaying all cards in category

--- src/DisplayQuestionsAndAnswers.js
+++ src/DisplayQuestionsAndAnswers.js
@@ -1,35 +1,40 @@
 import React from 'react';
-import Greeting from './Greeting'
+import StartOverNotice from './StartOverNotice';
 
-export default function DisplayQuestionsAndAnswers (props) {
+export default function DisplayQuestionsAndAnswers(props) {
 
-   if (props.flashcard === undefined) {
-    return <Greeting />
-   } 
-   if (props.correctAnswer === true) {
-    return (
-        <section>
-            <p className="question-text">That's right!</p>
-            <button onClick={props.showNextFlashcard} className="answer-text">Crushed it! Keep Going!</button>
-        </section>
-    )
-    } else if (props.correctAnswer === false) {
-    return (
-        <section>
-            <p className="question-text">Nice try, but incorrect. Let's add this question to your Study Bank for later!</p>
-            <button onClick={props.showNextFlashcard} className="answer-text">Keep Trying!</button>
-        </section>
-    )
-} 
    
-    return (
-        <section>
-            <p className="question-text">{props.flashcard.question}</p>
-            <section className="answer-choice-buttons-container">
-                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[0]}</button>
-                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[1]}</button>
-                <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[2]}</button>
-            </section>
-        </section>
-    )
+    
+        if (props.currentIndex === props.allFlashcards.length) {
+            return (
+            <StartOverNotice />)
+
+        }
+
+        if (props.correctAnswer) {
+         return (
+             <section>
+                 <p className="question-text">That's right!</p>
+                 <button onClick={props.showNextFlashcard} className="answer-text">Crushed it! Keep Going!</button>
+             </section>
+         )
+         } else if (props.correctAnswer === false) {
+         return (
+             <section>
+                 <p className="question-text">Nice try, but incorrect. Let's add this question to your Study Bank for later!</p>
+                 <button onClick={props.showNextFlashcard} className="answer-text">Keep Trying!</button>
+             </section>
+             )
+         } 
+
+       return (
+           <section>
+               <p className="question-text">{props.flashcard.question}</p>
+               <section className="answer-choice-buttons-container">
+                   <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[0]}</button>
+                   <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[1]}</button>
+                   <button onClick={props.validateAnswer} className="answer-text">{props.flashcard.answerChoices[2]}</button>
+               </section>
+           </section>
+       )
 }
