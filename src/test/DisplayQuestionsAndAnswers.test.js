18d50d09ce2b2a53591222510729cd285980b94f
“Kimberly <kimberly.achkio@gmail.com>
2019-02-26 13:16:33 -0700

    Passing tests for first two conditionals

--- src/test/DisplayQuestionsAndAnswers.test.js
+++ src/test/DisplayQuestionsAndAnswers.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import DisplayQuestionsAndAnswers from '../DisplayQuestionsAndAnswers';
 import { shallow } from 'enzyme';
 
+//this is what gets passed to this component:
+{/* <DisplayQuestionsAndAnswers 
+                    flashcard={this.props.flashcards[this.props.currentIndex]} 
+                    validateAnswer={this.validateAnswer}
+                    correctAnswer={this.state.correctAnswer}
+                    showNextFlashcard={this.showNextFlashcard}
+                    currentIndex={this.props.currentIndex}
+                    allFlashcards={this.props.flashcards}
+                />  */}
+
 const mockAllFlashcards = [
     {
         "id": "a257e858-36c6-11e9-b210-d663bd873d93",
@@ -13,21 +23,38 @@ const mockAllFlashcards = [
     }
 ]
 
-const mockIndex = 1;
+let mockIndex = 1;
+
+let mockCorrectAnswer;
 
 describe('DisplayQuestionsAndAnswers', () => {
     let wrapper;
 
-    beforeEach(() => {
-
-        wrapper = shallow (
-            <DisplayQuestionsAndAnswers />
-        )
-    });
 
     it('should match the snapshot if props.currentIndex === props.allFlashcards.length', () => {
-        let props.currentIndex = mockIndex;
-        let props.allFlashcards = mockAllFlashcards;
+
+        const wrapper = shallow( 
+            <DisplayQuestionsAndAnswers 
+                    flashcard={mockAllFlashcards[mockIndex]} 
+                    currentIndex={mockIndex}
+                    allFlashcards={mockAllFlashcards}
+                /> 
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should match the snapshot if props.correct answer', () => {
+        mockCorrectAnswer = true;
+        mockIndex = 2;
+
+        const wrapper = shallow(
+            <DisplayQuestionsAndAnswers 
+                    flashcard={mockAllFlashcards[mockIndex]} 
+                    correctAnswer={mockCorrectAnswer}
+                    currentIndex={mockIndex}
+                    allFlashcards={mockAllFlashcards}
+                />
+        );
         expect(wrapper).toMatchSnapshot();
     });
 
